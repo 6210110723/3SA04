e144d7441b84b9ad7ddc81c1433f25df1a090450
6210110723 <6210110723@psu.ac.th>
2022-08-27 21:33:37 +0700

    section 6

--- wt-app/App.js
+++ wt-app/App.js
@@ -1,109 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Dimensions, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import IonicIcon from 'react-native-vector-icons/Ionicons';
 
-import Northern from './screen/Northern';
-import Northeastern from './screen/Northeastern';
-import Central from './screen/Central';
-import Eastern from './screen/Eastern'
-import Western from './screen/Western'
-import Southern from './screen/Southern'
-import WeatherScreen from './screen/WeatherScreen'
-
-const fullScreenWidth = Dimensions.get('window').width;
+import { NavigationContainer, StackAction } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ZipCodeScreen from './screen/ZipCodeScreen';
+import WeatherScreen from './screen/WeatherScreen';
 
 const Stack = createStackNavigator()
-
-function NorthernStackScreen( ) {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Northern" component={Northern} />
-      <Stack.Screen name="Weather" component={WeatherScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function NortheasternStackScreen( ) {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Northeastern" component={Northeastern} />
-      <Stack.Screen name="Weather" component={WeatherScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function CentralStackScreen( ) {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Central" component={Central} />
-      <Stack.Screen name="Weather" component={WeatherScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function EasternStackScreen( ) {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Eastern" component={Eastern} />
-      <Stack.Screen name="Weather" component={WeatherScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function WesternStackScreen( ) {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Western" component={Western} />
-      <Stack.Screen name="Weather" component={WeatherScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function SouthernStackScreen( ) {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Southern" component={Southern} />
-      <Stack.Screen name="Weather" component={WeatherScreen} />
-    </Stack.Navigator>
-  );
-}
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-      screenOptions={({route}) => ({
-        headerTitle: () => <Text>Header</Text>,
-        tabBarIcon: ({focused, color, size, padding}) => {
-          let iconName;
-          if(route.name === 'Northern' || route.name === 'Northeastern' || route.name === 'Central' || route.name === 'Eastern' || route.name === 'Western' || route.name === 'Southern') {
-            iconName = focused ? 'earth' : 'earth-outline'
-          } 
-
-          return (
-            <IonicIcon 
-              name={iconName} 
-              size={size} 
-              color={color} 
-              style={{paddingBottom: padding}} 
-            />
-          );
-        },
-      })}
-      >
-        <Tab.Screen name="Northern" component={NorthernStackScreen} />
-        <Tab.Screen name="Northeastern" component={NortheasternStackScreen} />
-        <Tab.Screen name="Central" component={CentralStackScreen} />
-        <Tab.Screen name="Eastern" component={EasternStackScreen} />
-        <Tab.Screen name="Western" component={WesternStackScreen} />
-        <Tab.Screen name="Southern" component={SouthernStackScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />
+      </Stack.Navigator>
+    </NavigationContainer> 
   );
 }
 
