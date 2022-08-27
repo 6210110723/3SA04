3764a5dd96f0a02f4c5124d1389c93f42fee93ce
6210110723 <6210110723@psu.ac.th>
2022-08-27 21:30:52 +0700

    section 6

--- wt-app/components/Forcast.js
+++ wt-app/components/Forcast.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React  from "react";
+import { Text, View } from "react-native";
 
 export default function Forecast(props) {
-    return (
-        <View >
-            <Text>{props.main}</Text>
-            <Text>{props.description}</Text>
-            <View>
-            <Text>{props.temp}</Text>
-            <Text>°C</Text>
-            </View>
-        </View>
-    );
+  return (
+    <View style={{alignItems: "center"}}>
+      <Text style={{ fontSize: 20 }}>Province : {props.name}</Text>
+      <Text style={{ fontSize: 20 }}>Weather : {props.main}</Text>
+      <Text style={{ fontSize: 20 }}>Description : {props.description}</Text>
+      <Text style={{ fontSize: 20 }}>temp : {props.temp} °C</Text>
+    </View>
+  );
 }
--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,46 +1,48 @@
-import React from 'react';
-import { Text, ImageBackground, StyleSheet } from 'react-native'
-import Forecast from './Forcast';
+import React, { useEffect, useState } from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import Forecast from "./Forecast"
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
         main: '-',
         description: '-',
         temp: 0
     })
 
-    useEffect(() => {
-        console.log(`fetching data with zipCode = ${props.zipCode}`)
-        if (props.zipCode) {
-            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=a288a0f84c7311ca9af2c2918b638045`)
-                .then((response) => response.json())
-                .then((json) => {
-                    setForecastInfo({
-                        main: json.weather[0].main,
-                        description: json.weather[0].description,
-                        temp: json.main.temp
-                    });
-                })
-                .catch((error) => {
-                    console.warn(error);
-                });
-            }
-        }, [props.zipCode])
+   useEffect(() => { 
+
+   console.log(`fetching data with zipCode = ${props.zipCode}`);
+    if (props.zipCode) {
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=19d5f642cb405113038c9fa2f191a15d`)
+        .then((response) => response.json())
+        .then((json) => {
+          setForecastInfo({
+            name: json.name,
+            main: json.weather[0].main,
+            description: json.weather[0].description,
+            temp: json.main.temp});
+        })
+        .catch((error) => {
+          console.warn(error);
+        });
+    }
+  }, [props.zipCode])
 
     return (
-       <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-            <Text>Zip Code</Text>
-            <Text>{props.zipCode}</Text>
-            <Forecast {...forecastInfo}/>
-       </ImageBackground>
-    )
+    <View>
+    <ImageBackground source={require("../image/bg.jpg")} style={styles.backdrop}>
+       <Text style={{ fontWeight: 'bold' , fontSize: 24}}>Zip Code : {props.zipCode}</Text>
+       <Forecast {...forecastInfo} />
+     </ImageBackground>
+     </View>
+  );
 }
 
 const styles = StyleSheet.create({
     backdrop: {
-        flexDirection: 'column',
         alignItems: 'center',
         width: '100%',
         height: '100%'
-    }
-})
+    },
+}),
